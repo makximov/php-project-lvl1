27b42f6e504a0057e25cf7359c072a815c399def
makx <makx.work@gmail.com>
2021-01-08 16:11:10 +0200

    fix

--- bin/brain-even.php
+++ bin/brain-even.php
@@ -7,12 +7,12 @@ use function cli\prompt;
 
 require_once __DIR__ .'/../vendor/autoload.php';
 
-$name = Brain\Games\Cli::run();
+$playerName = Brain\Games\Cli::run();
 
 line('Answer "yes" if the number is even, otherwise answer "no".');
 
-$game = true;
-$count = 0;
+$gameContinue = true;
+$countRound = 0;
 
 const ANSWER_YES = 'yes';
 const ANSWER_NO = 'no';
@@ -22,23 +22,28 @@ do {
     $correctAnswer = $questionNumber & 1 ? ANSWER_NO : ANSWER_YES;
 
     line(sprintf('Question: %d', $questionNumber));
-    $answer = prompt('Your answer: ');
+    $playerAnswer = prompt('Your answer: ');
 
-    if ( ! in_array($answer, [ANSWER_YES, ANSWER_NO])) {
-        $game = false;
+    if ( ! in_array($playerAnswer, [ANSWER_YES, ANSWER_NO])) {
+        $gameContinue = false;
     } else {
 
-        if ($answer === $correctAnswer) {
+        if ($playerAnswer === $correctAnswer) {
             line('Correct!');
         } else {
-            line(sprintf('Let\'s try again, %s!', $name));
-            $game = false;
+            line(sprintf('\'%s\' is wrong answer ;(. Correct answer was \'%s\'.', $playerAnswer, $correctAnswer));
+            line(sprintf('Let\'s try again, %s!', $playerName));
+            $gameContinue = false;
         }
     }
 
-    $count++;
+    $countRound++;
 
-} while ($game && $count < 4);
+} while ($gameContinue && $countRound < 4);
+
+if ($gameContinue) {
+    line('Congratulations, %s!', $playerName);
+}
 
 
 
